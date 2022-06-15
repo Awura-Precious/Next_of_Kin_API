58db1470f85e716573228248cbf72e6930863010
Enoch Klufio <enochmk94@gmail.com>
2022-06-15 21:27:36 +0000

    refactor: update request id from uuid to nanoid

--- src/middlewares/handleRequestID.middleware.ts
+++ src/middlewares/handleRequestID.middleware.ts
@@ -1,8 +1,8 @@
 import { NextFunction, Request, Response } from 'express';
-import * as uuid from 'uuid';
+import { nanoid } from 'nanoid';
 
 const handleRequestID = async (req: Request, res: Response, next: NextFunction) => {
-	res.locals.requestID = uuid.v4();
+	res.locals.requestID = nanoid();
 	next();
 };
 
