9d600653ccdce500e10660addb53c3548d4e5331
Enoch Klufio <enochmk94@gmail.com>
2022-06-15 21:13:48 +0000

    feat: add error types

--- src/utils/errors/DBError.ts
+++ src/utils/errors/DBError.ts
@@ -1,9 +1,16 @@
+import { IContext } from '../../interfaces/ILogger.interface';
+import messages from '../../messages/db.messages';
+
 class DbError extends Error {
 	statusCode: number;
 
-	constructor(message: string) {
-		super(message);
-		this.name = this.constructor.name;
+	context: IContext;
+
+	constructor(message: string, context: IContext) {
+		super(message || messages.GENERAL_DB_ERROR);
+
+		this.name = 'DbError';
+		this.context = context;
 		this.statusCode = 500;
 	}
 }
